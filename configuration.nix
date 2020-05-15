44a4ad4df87dfefb3dd273bad30ae566d9848914
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-05-14 21:12:30 -0400

    Pin kernel version to 5.5
    Kernel 5.6 is currently broken on my setup on nightly.
    My GPU won't work at all.

--- configuration.nix
+++ configuration.nix
@@ -19,13 +19,16 @@
       ./host/g-word.nix
     ];
 
+  nixpkgs.overlays = [ (import ./overrides/dislocker.nix) ];
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
   system.stateVersion = "19.09"; # Did you read the comment?
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_5_5;
   
   boot.cleanTmpDir = true;
 
