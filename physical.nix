72f66908a692bbc9c76927e36300e358f6948a52
Nicolas Berbiche <nic.berbiche@gmail.com>
2020-05-26 12:58:03 -0400

    Fix udev rules inclusion

--- physical.nix
+++ physical.nix
@@ -1,22 +1,12 @@
 { config, pkgs, lib, ... }:
 
-let
-  config = lib.mkIf /* config.hardware.pulseaudio.enable */ false {
-    services.udev.extraRules = ''
-      ATTRS{idVendor}=="1038", ATTRS{idProduct}=="12ad", ENV{PULSE_PROFILE_SET}="steelseries-arctis-7-usb-audio.conf"
-      ATTRS{idVendor}=="1038", ATTRS{idProduct}=="12AD", ENV{PULSE_PROFILE_SET}="steelseries-arctis-7-usb-audio.conf"
-    '';
-  };
-in
 {
-  inherit (config);
-
   # Logitech
   hardware.logitech.enable = true;
   hardware.logitech.enableGraphical = false;
 
   # Steelseries headset
-  services.udev.extraRules = ''
+  services.udev.extraRules = lib.optionalString config.hardware.pulseaudio.enable ''
     ATTRS{idVendor}=="1038", ATTRS{idProduct}=="12ad", ENV{PULSE_PROFILE_SET}="steelseries-arctis-7-usb-audio.conf"
     ATTRS{idVendor}=="1038", ATTRS{idProduct}=="12AD", ENV{PULSE_PROFILE_SET}="steelseries-arctis-7-usb-audio.conf"
   '';
